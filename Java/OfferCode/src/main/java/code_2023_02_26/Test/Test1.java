92a25130c391ff205020592ec3ef0c1b6402e63c
xuk <2523590493@qq.com>
2023-02-26 20:51:04 +0800

    day5

---
+++ Java/OfferCode/src/main/java/code_2023_02_26/Test/Test1.java
@@ -0,0 +1,26 @@
+package code_2023_02_26.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('A',2);
+        map.put('B',1);
+        map.put('D',-2);
+
+        map.put('C',9);
+        System.out.println(map);
+
+        Set set =  map.keySet();
+        Object[] arr = set.toArray();
+        Arrays.sort(arr);
+        for(Object key : arr) {
+            System.out.println(key);
+        }
+
+    }
+}
